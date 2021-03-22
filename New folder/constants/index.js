1bb93ab6f6720a06145b5d680186f50179a12dee
hoangvudn <LehongphonG@0311!>
2021-03-22 10:15:26 +0700

    test

---
+++ New folder/constants/index.js
@@ -0,0 +1,20 @@
+// export const ADD_USER = "ADD_USER";
+// export const ADD_USER_SUCCESS = "ADD_USER_SUCCESS";
+// export const ADD_USER_ERROR = "ADD_USER_ERROR";
+
+export const GET_USER = "GET_USER";
+export const GET_USER_SUCCESS = "GET_USER_SUCCESS";
+export const GET_USER_ERROR = "GET_USER_ERROR";
+
+// export const GET_USER_DELETED = "GET_USER_DELETED";
+// export const USER_DELETED_SUCCESS = "USER_DELETED_SUCCESS ";
+// export const USER_DELETED_ERROR = "USER_DELETED_ERROR";
+
+// export const GET_USER_EDIT = "GET_USER_EDIT";
+// export const USER_EDIT_SUCCESS = "USER_EDIT_SUCCESS ";
+// export const USER_EDIT_ERROR = "USER_EDITED_ERROR";
+
+// export const GET_USER_UPDATE = "GET_USER_UPDATE";
+// export const USER_UPDATE_SUCCESS = "USER_UPDATE_SUCCESS ";
+// export const USER_UPDATE_ERROR = "USER_UPDATE_ERROR";
+
